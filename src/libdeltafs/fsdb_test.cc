9491e6bc38c6c671c4d6f61092a87b9b9ab47ea0
Qing Zheng <zhengq@cs.cmu.edu>
2020-06-01 03:11:15 -0400

    allow setting sst write buffer size in fs db bench

--- src/libdeltafs/fsdb_test.cc
+++ src/libdeltafs/fsdb_test.cc
@@ -470,12 +470,16 @@ class Benchmark {
             FLAGS_dboptions.enable_io_monitoring);
     fprintf(stdout, "WAL off:            %d\n",
             FLAGS_dboptions.disable_write_ahead_logging);
+    fprintf(stdout, "WAL write buffer:   %d KB\n",
+            int(FLAGS_dboptions.write_ahead_log_buffer >> 10));
     fprintf(stdout, "Lsm compaction off: %d\n",
             FLAGS_dboptions.disable_compaction);
     fprintf(stdout, "Mem table size:     %d MB\n",
             int(FLAGS_dboptions.memtable_size >> 20));
     fprintf(stdout, "Table size:         %d MB\n",
             int(FLAGS_dboptions.table_size >> 20));
+    fprintf(stdout, "Table write buffer: %d KB\n",
+            int(FLAGS_dboptions.table_buffer >> 10));
     fprintf(stdout, "Level factor:       %d\n", FLAGS_dboptions.level_factor);
     fprintf(stdout, "L1 trigger:         %d\n",
             FLAGS_dboptions.l1_compaction_trigger);
@@ -1017,6 +1021,9 @@ void BM_Main(int* const argc, char*** const argv) {
     } else if (sscanf((*argv)[i], "--table_file_size=%dM%c", &n, &junk) == 1) {
       pdlfs::FLAGS_dboptions.table_size = n << 20;
       pdlfs::FLAGS_dboptions.memtable_size = (n << 20) << 1;
+    } else if (sscanf((*argv)[i], "--table_write_buffer=%dK%c", &n, &junk) ==
+               1) {
+      pdlfs::FLAGS_dboptions.table_buffer = n << 10;
     } else if (sscanf((*argv)[i], "--block_size=%dK%c", &n, &junk) == 1) {
       pdlfs::FLAGS_dboptions.block_size = n << 10;
     } else if (sscanf((*argv)[i], "--l1_compaction_trigger=%d%c", &n, &junk) ==
