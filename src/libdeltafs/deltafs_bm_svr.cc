32e6599bbde62edbca51505584aebf10e01888a8
Qing Zheng <zhengq@cs.cmu.edu>
2020-06-14 02:01:01 -0400

    print db io info on exit

--- src/libdeltafs/deltafs_bm_svr.cc
+++ src/libdeltafs/deltafs_bm_svr.cc
@@ -31,6 +31,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include "env_wrapper.h"
 #include "fs.h"
 #include "fsdb.h"
 #include "fsis.h"
@@ -496,6 +497,31 @@ class Server {
       svrs_[i]->Close();
     }
     MPI_Barrier(MPI_COMM_WORLD);
+    if (FLAGS_dbopts.enable_io_monitoring) {
+      fprintf(stdout, "Total random reads: %llu ",
+              static_cast<unsigned long long>(
+                  fsdb_->GetDbEnv()->TotalRndTblReads()));
+      fprintf(stdout, "(Avg read size: %.1fK, total bytes read: %llu)\n",
+              1.0 * fsdb_->GetDbEnv()->TotalRndTblBytesRead() / 1024.0 /
+                  fsdb_->GetDbEnv()->TotalRndTblReads(),
+              static_cast<unsigned long long>(
+                  fsdb_->GetDbEnv()->TotalRndTblBytesRead()));
+      fprintf(stdout, "Total sequential bytes read: %llu ",
+              static_cast<unsigned long long>(
+                  fsdb_->GetDbEnv()->TotalSeqTblBytesRead()));
+      fprintf(stdout, "(Avg read size: %.1fK)\n",
+              1.0 * fsdb_->GetDbEnv()->TotalSeqTblBytesRead() / 1024.0 /
+                  fsdb_->GetDbEnv()->TotalSeqTblReads());
+      fprintf(stdout, "Total bytes written: %llu ",
+              static_cast<unsigned long long>(
+                  fsdb_->GetDbEnv()->TotalTblBytesWritten()));
+      fprintf(stdout, "(Avg write size: %.1fK)\n",
+              1.0 * fsdb_->GetDbEnv()->TotalTblBytesWritten() / 1024.0 /
+                  fsdb_->GetDbEnv()->TotalTblWrites());
+    }
+    fprintf(stdout, " - Db stats: >>>\n%s\n", fsdb_->GetDbStats().c_str());
+    fprintf(stdout, " - L0 stats: >>>\n%s\n",
+            fsdb_->GetDbLevel0Events().c_str());
     if (FLAGS_rank == 0) {
       puts("Bye!");
     }
