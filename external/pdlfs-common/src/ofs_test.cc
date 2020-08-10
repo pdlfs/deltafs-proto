77416c0782224ffb27cb81678e88e46bfd972764
Qing Zheng <zhengq@cs.cmu.edu>
2020-08-09 22:23:48 -0400

    clean ofs test

--- external/pdlfs-common/src/ofs_test.cc
+++ external/pdlfs-common/src/ofs_test.cc
@@ -19,6 +19,7 @@ namespace pdlfs {
 class OFS {
  public:
   OFS() {
+    fsetpath_ = "/mnt/fset";
     root_ = test::PrepareTmpDir("ofs_test");
     osd_ = Osd::FromEnv(root_.c_str());
     ofs_ = new Ofs(options_, osd_);
@@ -29,21 +30,46 @@ class OFS {
     delete osd_;
   }
 
-  bool Mounted() {
-    bool r = ofs_->FileSetExists("/mnt/fset");
-    return r;
-  }
+  bool Mounted() { return ofs_->FileSetExists(fsetpath_.c_str()); }
 
-  Status Mount() {
-    Status s = ofs_->MountFileSet(mount_opts_, "/mnt/fset");
-    return s;
-  }
+  Status Mount() { return ofs_->MountFileSet(mount_opts_, fsetpath_.c_str()); }
 
   Status Unmount() {
-    Status s = ofs_->UnmountFileSet(unmount_opts_, "/mnt/fset");
+    return ofs_->UnmountFileSet(unmount_opts_, fsetpath_.c_str());
+  }
+
+  Status Access(const char* fname) {
+    SequentialFile* file;
+    std::string f = fsetpath_ + "/" + fname;
+    Status s = ofs_->NewSequentialFile(f.c_str(), &file);
+    if (s.ok()) {
+      delete file;
+    }
     return s;
   }
 
+  Status Create(const char* fname) {
+    WritableFile* file;
+    std::string f = fsetpath_ + "/" + fname;
+    Status s = ofs_->NewWritableFile(f.c_str(), &file);
+    if (s.ok()) {
+      file->Close();
+      delete file;
+    }
+    return s;
+  }
+
+  Status Delete(const char* fname) {
+    std::string f = fsetpath_ + "/" + fname;
+    return ofs_->DeleteFile(f.c_str());
+  }
+
+  bool Exists(const char* fname) {
+    std::string f = fsetpath_ + "/" + fname;
+    return ofs_->FileExists(f.c_str());
+  }
+
+  std::string fsetpath_;
   std::string root_;
   MountOptions mount_opts_;
   UnmountOptions unmount_opts_;
@@ -53,7 +79,7 @@ class OFS {
 };
 
 TEST(OFS, Empty) {
-  ASSERT_TRUE(!ofs_->FileExists("/mnt/fset/a"));
+  ASSERT_TRUE(!ofs_->FileExists(fsetpath_.c_str()));
   ASSERT_TRUE(!Mounted());
   ASSERT_NOTFOUND(Unmount());
 }
@@ -82,19 +108,26 @@ TEST(OFS, MountUnmount) {
 
 TEST(OFS, CreateDeleteFile) {
   ASSERT_OK(Mount());
-  WritableFile* wf;
-  ASSERT_OK(ofs_->NewWritableFile("/mnt/fset/a", &wf));
-  wf->Close();
-  delete wf;
+  ASSERT_OK(Create("a"));
+  ASSERT_OK(Create("b"));
   ASSERT_OK(Unmount());
   ASSERT_OK(Mount());
-  SequentialFile* sf;
-  ASSERT_OK(ofs_->NewSequentialFile("/mnt/fset/a", &sf));
-  delete sf;
-  ASSERT_OK(ofs_->DeleteFile("/mnt/fset/a"));
+  ASSERT_OK(Access("a"));
+  ASSERT_OK(Access("b"));
+  ASSERT_OK(Delete("a"));
+  ASSERT_OK(Delete("b"));
+  ASSERT_OK(Create("c"));
+  ASSERT_OK(Create("d"));
   ASSERT_OK(Unmount());
   ASSERT_OK(Mount());
-  ASSERT_TRUE(!ofs_->FileExists("/mnt/fset/a"));
+  ASSERT_TRUE(!Exists("a"));
+  ASSERT_TRUE(!Exists("b"));
+  ASSERT_OK(Delete("c"));
+  ASSERT_OK(Delete("d"));
+  ASSERT_OK(Unmount());
+  ASSERT_OK(Mount());
+  ASSERT_TRUE(!Exists("c"));
+  ASSERT_TRUE(!Exists("d"));
   unmount_opts_.deletion = true;
   ASSERT_OK(Unmount());
 }
